2ed32cb4dfa5477a6242d835c43f272cc1bd220a
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-13 12:35:24 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -36,6 +36,9 @@ class Transfer
   
   def reverse_transfer
     if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
     end
   end
 end
