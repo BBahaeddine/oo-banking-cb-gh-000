698a477f0e15931eb173e3b4c99fd2e4075cf096
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-13 12:25:50 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,12 +3,12 @@ class Transfer
   attr_accessor :sender
   attr_accessor :receiver
   attr_accessor :status
-  attr_accessor :transfer_amount
+  attr_accessor :amount
   
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
-    @transfer_amount = amount
+    @amount = amount
     @status = "pending"
     
   end
