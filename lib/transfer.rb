6311d7ec857058a663b2b166d200c5b2edf503bb
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-13 12:30:14 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,6 +20,15 @@ class Transfer
   end
   
   def execute_transaction
+    if valid?
+      if @status == "pending"
+        @receiver.balance -= @amount
+        @sender.balance += @amount
+        @status = "completed"
+      end
+    else
+      "Transaction rejected. Please check your account balance."
+    end
   end
   
   def reverse_transfer
