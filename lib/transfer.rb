05f4904d4547ed02ff90099f27abfa201d45858b
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-13 12:31:32 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,7 +27,7 @@ class Transfer
         @status = "complete"
       end
     else
-      "Transaction rejected. Please check your account balance."
+      return "Transaction rejected. Please check your account balance."
     end
   end
   
