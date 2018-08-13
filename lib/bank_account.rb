cb6cbf8e41d6a10cd0f83278243c7388a617707c
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-13 12:20:39 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -8,5 +8,18 @@ class BankAccount
     @balance = 1000
     @status = "open"
   end
+  def deposit(money)
+    @balance += money
+  end
+  
+  
+  def display_balance
+  end
+  
+  def valid?
+  end
+  
+  def close_account
+  end
 
 end
